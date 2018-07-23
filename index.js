e6e20dff26fa4d0a42e5f5e4332af18eb637d077
husizhang <hsz@meitu.com>
2018-07-23 14:11:20 +0800

    first commit

--- index.js
+++ index.js
@@ -1,93 +1,90 @@
-#!/usr/bin/env node
-const path=require('path');
 const http = require('http');
-const https = require('https');
-const fs = require('fs');
-const Koa = require('koa');
-const app = new Koa();
-const program = require('commander');
-const template = require('art-template');
-const static = require('koa-send');
-const bodyParser = require('koa-body')
-
-global.CONFIG={};
-const proxyUrl=require('../proxy/proxy');
-const rewriteUrl=require('../proxy/rewrite');
-const mockAction=require('../proxy/mock');
-const {fillValue}=require('../proxy/util')
-
-
-program
-  .version('1.0', '-v, --version')
-  .option('-q, --quiet', '不打开管理页面')
-  .option('-p, --port [port]' , '设置http端口',/^\d+$/,80)
-  .option('-s, --safe [safe]' , '设置https端口',/^\d+$/,443)
-  .option('-S, --static [static]','设置静态代理目录,多个目录"::"分隔',(val)=>val.split("::"))
-  .parse(process.argv);
-if(program.static){
-  global.CONFIG["static_path"]=program.static.map((s)=>{return path.resolve(process.cwd(),s)})
-}
-app.use(bodyParser({multipart: true}))
-app.use(async (ctx,next)=>{
-	if(ctx.url=="/_mtproxy_/getConfig" && ctx.method =="GET"){
-    ctx.body = JSON.stringify(global.CONFIG);
-    return;
-	}
-  await next();
-});
-app.use(async (ctx,next)=>{
-  if(ctx.url=="/_mtproxy_/delConfig" && ctx.method =="POST"){
-      let data=ctx.request.body;
-      if(data.path && /^global\b/.test(data.path)){
-        if(/^global[\b]+_readonly/.test(data.path)){
-            ctx.body=JSON.stringify({
-              status:-1,
-              message:"只读属性不能删除"
-            })
-        }else{
-          var str=data.path.replace(/^global/,"global.CONFIG"),
-              message="",status=0,last,
-              reg1=/\[\s*[\'\"]{0,1}(.+)[\'\"]{0,1}\s*\]$/,
-              reg2=/\b([\w]+)$/;
-          str=str.replace(reg1,''),last=RegExp.$1;
-          if(!last){
-              lstr=str.replace(reg2,''),last=RegExp.$1;
-          }
-          try{
-            var obj=new Function('return '+str)();
-            if(obj instanceof Array){
-              obj.splice(last,1)
-            }else{
-              delete obj[last];
-            }
-          }catch(e){
-              console.log(e)
-              message="没有找到被删除的属性"
-          }
-          ctx.body=JSON.stringify({
-              status:0,
-              message:message||"删除成功"
-          })
-        }
-        return;
+const https =require('https');
+const qs = require('querystring');
+const options = {
+    key: fs.readFileSync(path.join(__dirname,'./keys/key.pem')),
+    cert: fs.readFileSync(path.join(__dirname,'./keys/server.crt'))
+};
+const errParamTips=`参数不正确,正确(GET)请求方式：[ http://127.0.0.1/_mtproxy_/addproxy?pattern=*.test.*.com&dest=qa.xxx.com ],非浏览器请求且含特殊字符请使用encodeURIComponent编码`
+module._proxyMaps=module._proxyMaps||[];
+module.exports=function(opt={}){
+  opt=Object.assign({
+    port:80,
+    safe:false
+  },opt);
+  let proxy=(req, res, protocol)=>{
+    let send=(response)=>{
+      let responseText="";
+      if(Object.prototype.toString.call(response)==="[object Object]"){
+        responseText=JSON.Stringify(response);
+        res.setHeader('Content-Type', 'application/json');
       }
-  }
-  await next();
-});
-app.use(proxyUrl)
-app.use(async (ctx,next)=>{
-  let sp=global.CONFIG["static_path"];
-  if(sp) {
-    for (var i = 0; i < sp.length; i++) {
-      try{
-        await static(ctx, ctx.path,{ root: sp[i] });
-      }catch(err){
-        if (err.status !== 404){
-          console.log(err)
-          throw err
-        }
+      res.end(responseText)
+    };
+    let proxyMaps=module._proxyMaps,url=req.url;
+    if(/^\/_mtproxy_\/setproxy/.test(url)){
+      let queryStr=url.split("?")[1];
+      if(!queryStr) return send({status:-1,msg:errParamTips});
+      let {pattern,dest}=qs.parse(queryStr);
+      if(!pattern || !/^[\w\-\.\:]+$/.test(dest)){return send({status:-1,msg:errParamTips})}
+      let reg=new RegExp(pattern.replace(/([^\w\*])/g,"\\$1").replace(/\*/g,".*"),"i")
+      let oldIndex=proxyMaps.findIndex((item)=>{return item.pattern===pattern});
+      if(oldIndex!=-1){
+        proxyMaps[oldIndex]={pattern,dest};
+      }else{
+        proxyMaps.push({pattern,dest,reg});
+      }
+      send({status:0,msg:`${oldIndex==-1?"新增":"更新"}一条规则:[${pattern+":"+dest}]`});
+      return;
+    }
+    if(/^\/_mtproxy_\/getproxy/.test(url)){
+      send({status:0,data:proxyMaps})
+      return;
+    }
+    let host=req.headers.host;
+    for (var i = 0; i < proxyMaps.length; i++) {
+      if(proxyMaps[i].reg.test(host)){
+        let dest=proxyMaps[i].dest.split(":");
+        req.setHeader('Host',dest);
+        req.pipe(protocol.request({host:dest[0],port:dest[1]||80,path:url}),{end:true}).pipe(res);
+        res.end();
+        return;
       }
     }
   }
-})
-
+  let serve=(server,port)=>{
+    server.on("error",(err)=>{
+      if(err.message.indexOf("EADDRINUSE")!=-1){
+        http.request("http://127.0.0.1:"+opt.port+"/_mtproxy_/getproxy",(response)=>{
+          res.setEncoding('utf8');
+          let body="";
+          res.on('data', (chunk) => {
+            body+=chunk;
+          });
+          res.on('end', () => {
+            try{
+              if(JSON.parse(body).status!=0) throw Error('EADDRINUSE');
+            }catch(e){
+              server.emit("error",err)
+            }
+            server.listen(port)
+          });
+        }).on("error",()=>{
+          server.emit("error",err)
+        })
+      }
+    }
+  }
+  let httpServer=http
+    .createServer((req,res)=>{
+      proxy(req,res,http)
+    })
+    .on(serve,opt.port);
+  if(opt.safe){
+    let httpsServer=https
+      .createServer(options,(req,res)=>{
+        proxy(req,res,https)
+      })
+      .on(serve,opt.safe);
+  }
+}
